65828c373a09f8838a053e311e34be9939a647e7
Sebustian <sebberamsay@hotmail.com>
2017-06-08 16:27:47 +0200

    hasu user password b4 saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,27 @@
 const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
+var password='123abc!';
+
+/*bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});*/
+
+var hp='$2a$10$mKkLgBWQLomsQ4vL5Xkx8eqe9GMNRYOeFzjlOaGp0L/9PrCMK/rkm';
+
+bcrypt.compare(password,hp, (err,res)=>{
+  console.log(res);
+});
+
+
+
+
+
+/*
 var data = {
   id:4
 };
@@ -9,7 +29,7 @@ var token = jwt.sign(data,'123abc');
 console.log(token);
 //jwt.verify
 
-/*var message = 'I am user number 3';
+var message = 'I am user number 3';
 var hash=SHA256(message).toString();
 
 console.log(message);
